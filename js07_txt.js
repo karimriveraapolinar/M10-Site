5c7b9af03f2463608dee7bd71179fe914517aa84
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2025-04-04 11:53:44 -0400

    done i guess

--- js07_txt.js
+++ js07_txt.js
@@ -4,11 +4,105 @@
       Chapter case   
 
       Word Cloud   Generator
-      Author: 
-      Date:       
+      Author: Karim Rivera-Apolinar
+      Date:  4/4/25      
 
       Filename:       js07.js
  */
+
+document.getElementById("getFile").onchange = function() {
+      // Retrieve information about the selected file
+      let userFile = this.files[0];
+
+      // Verify that a text file is selected
+      try {
+            let isText = userFile.type.startsWith("text");
+            if (!isText) {
+                  throw userFile.name + " is not a text file";
+      }
+      // Read the contents of the selected file
+      let fr = new FileReader();
+      fr.readAsText(userFile);
+      // Once the file has finished loading, display in the page
+      let sourceDoc = document.getElementById("wc_document");
+      fr.onload=function(){
+            sourceDoc.innerHTML = fr.result;
+
+            // Store the text of the document, removing HTML tags
+            let sourceText = sourceDoc.textContent;
+            // Generate the word cloud
+            wordCloud(sourceText);
+            }
+      }   
+      
+      // Alert the user to select a text file
+      catch(err) {
+            window.alert(err);
+      }
+      function wordCloud(sourceText) {
+            // Convert the source text to lowercase
+            // and remove leading and trailing whitespace
+            sourceText = sourceText.toLowerCase();
+            sourceText = sourceText.trim();
+
+            // Leave only alphabet characters and whitespace in the text
+            let alphaRegx = /[^a-zA-Z\s]/g;
+            sourceText = sourceText.replace(alphaRegx, "");
+
+            // Remove stop words from the text
+            for (let i = 0; i < stopWords.length; i++) {
+                  let stopRegx = new RegExp("\\b"+stopWords[i]+"\\b", "g");
+                  sourceText = sourceText.replace(stopRegx, "");
+            }
+            // Place the remaining words in array
+            let words = sourceText.split(/\s+/g);
+
+            // Sort the words in alphabetical order
+            words.sort();
+
+            // Create an 2D array in which each item is array
+            // containing a word and its duplicate count
+            let unique = [ [words[0], 1] ];
+
+            // Keep an index of the unique words
+            let uniqueIndex = 0;
+
+            for (let i = 1; i < words.length; i++ ) {
+                  if (words[i] === words[i-1] ) {
+                  // Increase the duplicate count by 1
+                  unique[uniqueIndex][1]++;
+                  } else {
+                  // Add a new word to the unique array
+                  uniqueIndex++;
+                  unique[uniqueIndex] = [words[i], 1];
+                  }
+            // Sort by descending order of duplicate count
+            unique.sort(byDuplicate);
+            function byDuplicate(a, b) {
+            return b[1]-a[1];
+            }
+            // Keep the Top 100 words
+            unique = unique.slice(0, 100);
+
+            // Find the duplicates of the most-repeated word
+            let maxCount = unique[0][1];
+            // Sort the word list in alphabetic order
+            unique.sort();
+
+            // Reference the word cloud box
+            let cloudBox = document.getElementById("wc_cloud");
+            cloudBox.innerHTML = "";
+
+            // Size each word based on its usage
+            for (let i = 0; i < unique.length; i++) {
+                  let word = document.createElement("span");
+                  word.textContent = unique[i][0];
+                  word.style.fontSize = unique[i][1]/maxCount + "em";
+                  cloudBox.appendChild(word);
+                  }
+            }
+      };
+}
 
 
 
@@ -85,4 +179,3 @@ let stopWords = ["a", "about", "above", "across", "after", "afterwards", "again"
                  "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", 
                  "with", "within", "without", "would", "year", "years", "yet", "you", 
                  "your", "yours", "yourself", "yourselves"];
-                 
